c799818c45ce588697c3e992241e65e62004269d
zenxxn <bunbun2438@naver.com>
2023-07-12 18:13:02 +0900

    test2

--- Main.java
+++ Main.java
@@ -7,4 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi it is CHS branch");
     }
+    public void zenxxn(){
+        System.out.println("Hello I'm zenxxn");
+    }
 }
